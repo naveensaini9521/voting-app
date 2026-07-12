2193ce319131e41d1ebbeb9f9412639ad9e9aa1b
naveen saini <naveensaini6378@gmail.com>
2026-07-12 14:07:07 +0530

    change docker command

--- start.sh
+++ start.sh
@@ -7,6 +7,9 @@ minikube addons enable ingress
 
 minikube addons enable metrics-server
  
+docker pull naveen9521/vote-app-project:latest
+minikube image load naveen9521/vote-app-project:latest
+
 kubectl create namespace voting-app
  
 kubectl apply -f k8s-specifications/ -n voting-app
@@ -14,4 +17,3 @@ kubectl apply -f k8s-specifications/ -n voting-app
 echo "127.0.0.1 votingapp.local" | sudo tee -a /etc/hosts
  
 minikube dashboard
- 
